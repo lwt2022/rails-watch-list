d5848a1fb94ebbb45fdb9380c85f5d181e84888f
antonio jurado <iberico.cortador@gmail.com>
2022-07-15 20:15:10 +0900

    bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,28 @@
 class BookmarksController < ApplicationController
-  get :bookmark to @lists
+
+  def new
+    #new -> prepare empty bookmark to be filled
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    @movies = Movie.all
+  end
+
+  def create
+    # create -> populate attributes with the user input and save to DB
+
+    # params = {"authenticity_token"=>"[FILTERED]", "bookmark"=>{"comment"=>"hello world", "movie"=>"1"}, "commit"=>"Create Bookmark", "list_id"=>"2"}
+
+    @list = List.find(params[:list_id])
+    # recreate the user input as isntances of movie and list
+    @movie = Movie.find(params[:bookmark][:movie])
+    @comment = params[:bookmark][:comment]
+    # create a new instance of bookmark with the instances of movie list , and comment
+    @bookmark = Bookmark.new(movie: @movie, list: @list, comment: @comment)
+    @bookmark.save
+    # .save (will generate an squl to the database)
+    redirect_to "/lists/"
+
+
+  end
+
 end
